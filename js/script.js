f47521f33c83ac22f2509c4912362dc79bc0b403
Uzurch1k <galcenkovlad7@gmail.com>
2023-11-09 22:22:34 +0200

    fix menu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let menuBtn = document.querySelector('.header-menu-btn');
 let menu = document.querySelector('.mob-menu');
 
-let modalBtn = document.querySelector('.footer-subscribe-btn');
+let modalBtn = document.querySelector('.hero-btn');
 let modalBtnClose = document.querySelector('.modal-close-btn');
 let modal = document.querySelector('.backdrop');
 
@@ -17,4 +17,29 @@ modalBtn.addEventListener('click', function () {
 modalBtnClose.addEventListener('click', function () {
   modalBtnClose.classList.toggle('active');
   modal.classList.toggle('active');
+});
+
+//=========modalBtnLink;==========
+let modalBtnLink = document.querySelectorAll('.item');
+let modalLink = document.querySelector('.mob-menu');
+
+let firstItem = modalBtnLink[0];
+let twoItem = modalBtnLink[1];
+let threeItem = modalBtnLink[2];
+
+//=========modalBtnLink==========
+firstItem.addEventListener('click', function () {
+  firstItem.classList.toggle('active');
+  modalLink.classList.toggle('active');
+  menuBtn.classList.toggle('active');
+});
+twoItem.addEventListener('click', function () {
+  twoItem.classList.toggle('active');
+  modalLink.classList.toggle('active');
+  menuBtn.classList.toggle('active');
+});
+threeItem.addEventListener('click', function () {
+  threeItem.classList.toggle('active');
+  modalLink.classList.toggle('active');
+  menuBtn.classList.toggle('active');
 });
